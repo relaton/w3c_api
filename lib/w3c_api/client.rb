272944766571234745d106fc5ca544860930ae59
Ronald Tse <ronald.tse@ribose.com>
2025-07-03 18:56:59 +0800

    fix: handle the case when there are no options

--- lib/w3c_api/client.rb
+++ lib/w3c_api/client.rb
@@ -7,95 +7,101 @@ require_relative 'hal'
 
 module W3cApi
   class Client
-    def specifications(options = {})
-      Hal.instance.register.fetch(:specification_index, **options)
+    def specifications(options = nil)
+      fetch_resource(:specification_index, **(options || {}))
     end
 
     def specification(shortname, options = {})
-      Hal.instance.register.fetch(:specification_resource, shortname: shortname, **options)
+      fetch_resource(:specification_resource, shortname: shortname, **options)
     end
 
     def specification_versions(shortname, options = {})
-      Hal.instance.register.fetch(:specification_resource_version_index, shortname: shortname, **options)
+      fetch_resource(:specification_resource_version_index,
+                     shortname: shortname, **options)
     end
 
     def specification_version(shortname, version, options = {})
-      Hal.instance.register.fetch(
+      fetch_resource(
         :specification_resource_version_resource,
         shortname: shortname,
         version: version,
-        **options
+        **options,
       )
     end
 
     def specifications_by_status(status, options = {})
-      Hal.instance.register.fetch(:specification_by_status_index, status: status, **options)
+      fetch_resource(:specification_by_status_index, status: status, **options)
     end
 
     def specification_supersedes(shortname, options = {})
-      Hal.instance.register.fetch(:specification_supersedes_index, shortname: shortname, **options)
+      fetch_resource(:specification_supersedes_index, shortname: shortname,
+                                                      **options)
     end
 
     def specification_superseded_by(shortname, options = {})
-      Hal.instance.register.fetch(:specification_superseded_by_index, shortname: shortname, **options)
+      fetch_resource(:specification_superseded_by_index, shortname: shortname,
+                                                         **options)
     end
 
     # New methods for editors and deliverers
 
     def specification_editors(shortname, options = {})
-      Hal.instance.register.fetch(:specification_editors_index, shortname: shortname, **options)
+      fetch_resource(:specification_editors_index, shortname: shortname,
+                                                   **options)
     end
 
     def specification_deliverers(shortname, options = {})
-      Hal.instance.register.fetch(:specification_deliverers_index, shortname: shortname, **options)
+      fetch_resource(:specification_deliverers_index, shortname: shortname,
+                                                      **options)
     end
 
     # Series methods
 
     def series(options = {})
-      Hal.instance.register.fetch(:serie_index, **options)
+      fetch_resource(:serie_index, **options)
     end
 
     def series_by_shortname(shortname, options = {})
-      Hal.instance.register.fetch(:serie_resource, shortname: shortname, **options)
+      fetch_resource(:serie_resource, shortname: shortname, **options)
     end
 
     def series_specifications(shortname, options = {})
-      Hal.instance.register.fetch(:serie_specification_resource, shortname: shortname, **options)
+      fetch_resource(:serie_specification_resource, shortname: shortname,
+                                                    **options)
     end
 
     # Group methods
 
     def groups(options = {})
-      Hal.instance.register.fetch(:group_index, **options)
+      fetch_resource(:group_index, **options)
     end
 
     def group(id, options = {})
-      Hal.instance.register.fetch(:group_resource, id: id, **options)
+      fetch_resource(:group_resource, id: id, **options)
     end
 
     def group_specifications(id, options = {})
-      Hal.instance.register.fetch(:group_specifications_index, id: id, **options)
+      fetch_resource(:group_specifications_index, id: id, **options)
     end
 
     def group_users(id, options = {})
-      Hal.instance.register.fetch(:group_users_index, id: id, **options)
+      fetch_resource(:group_users_index, id: id, **options)
     end
 
     def group_charters(id, options = {})
-      Hal.instance.register.fetch(:group_charters_index, id: id, **options)
+      fetch_resource(:group_charters_index, id: id, **options)
     end
 
     def group_chairs(id, options = {})
-      Hal.instance.register.fetch(:group_chairs_index, id: id, **options)
+      fetch_resource(:group_chairs_index, id: id, **options)
     end
 
     def group_team_contacts(id, options = {})
-      Hal.instance.register.fetch(:group_team_contacts_index, id: id, **options)
+      fetch_resource(:group_team_contacts_index, id: id, **options)
     end
 
     def group_participations(id, options = {})
-      Hal.instance.register.fetch(:group_participations_index, id: id, **options)
+      fetch_resource(:group_participations_index, id: id, **options)
     end
 
     # User methods
@@ -107,91 +113,103 @@ module W3cApi
     # end
 
     def user(hash, options = {})
-      Hal.instance.register.fetch(:user_resource, hash: hash, **options)
+      fetch_resource(:user_resource, hash: hash, **options)
     end
 
     def user_groups(hash, options = {})
-      Hal.instance.register.fetch(:user_groups_index, hash: hash, **options)
+      fetch_resource(:user_groups_index, hash: hash, **options)
     end
 
     def user_affiliations(hash, options = {})
-      Hal.instance.register.fetch(:user_affiliations_index, hash: hash, **options)
+      fetch_resource(:user_affiliations_index, hash: hash, **options)
     end
 
     def user_participations(hash, options = {})
-      Hal.instance.register.fetch(:user_participations_index, hash: hash, **options)
+      fetch_resource(:user_participations_index, hash: hash, **options)
     end
 
     def user_chair_of_groups(hash, options = {})
-      Hal.instance.register.fetch(:user_chair_of_groups_index, hash: hash, **options)
+      fetch_resource(:user_chair_of_groups_index, hash: hash, **options)
     end
 
     def user_team_contact_of_groups(hash, options = {})
-      Hal.instance.register.fetch(:user_team_contact_of_groups_index, hash: hash, **options)
+      fetch_resource(:user_team_contact_of_groups_index, hash: hash, **options)
     end
 
     def user_specifications(hash, options = {})
-      Hal.instance.register.fetch(:user_specifications_index, hash: hash, **options)
+      fetch_resource(:user_specifications_index, hash: hash, **options)
     end
 
     # Translation methods
 
     def translations(options = {})
-      Hal.instance.register.fetch(:translation_index, **options)
+      fetch_resource(:translation_index, **options)
     end
 
     def translation(id, options = {})
-      Hal.instance.register.fetch(:translation_resource, id: id, **options)
+      fetch_resource(:translation_resource, id: id, **options)
     end
 
     # Affiliation methods
 
     def affiliations(options = {})
-      Hal.instance.register.fetch(:affiliation_index, **options)
+      fetch_resource(:affiliation_index, **options)
     end
 
     def affiliation(id, options = {})
-      Hal.instance.register.fetch(:affiliation_resource, id: id, **options)
+      fetch_resource(:affiliation_resource, id: id, **options)
     end
 
     def affiliation_participants(id, options = {})
-      Hal.instance.register.fetch(:affiliation_participants_index, id: id, **options)
+      fetch_resource(:affiliation_participants_index, id: id, **options)
     end
 
     def affiliation_participations(id, options = {})
-      Hal.instance.register.fetch(:affiliation_participations_index, id: id, **options)
+      fetch_resource(:affiliation_participations_index, id: id, **options)
     end
 
     # Ecosystem methods
 
     def ecosystems(options = {})
-      Hal.instance.register.fetch(:ecosystem_index, **options)
+      fetch_resource(:ecosystem_index, **options)
     end
 
     def ecosystem(id, options = {})
-      Hal.instance.register.fetch(:ecosystem_resource, id: id, **options)
+      fetch_resource(:ecosystem_resource, id: id, **options)
     end
 
     def ecosystem_groups(shortname, options = {})
-      Hal.instance.register.fetch(:ecosystem_groups_index, shortname: shortname, **options)
+      fetch_resource(:ecosystem_groups_index, shortname: shortname, **options)
     end
 
     def ecosystem_evangelists(shortname, options = {})
-      Hal.instance.register.fetch(:ecosystem_evangelists_index, shortname: shortname, **options)
+      fetch_resource(:ecosystem_evangelists_index, shortname: shortname,
+                                                   **options)
     end
 
     def ecosystem_member_organizations(shortname, options = {})
-      Hal.instance.register.fetch(:ecosystem_member_organizations_index, shortname: shortname, **options)
+      fetch_resource(:ecosystem_member_organizations_index,
+                     shortname: shortname, **options)
     end
 
     # Participation methods
 
     def participation(id, options = {})
-      Hal.instance.register.fetch(:participation_resource, id: id, **options)
+      fetch_resource(:participation_resource, id: id, **options)
     end
 
     def participation_participants(id, options = {})
-      Hal.instance.register.fetch(:participation_participants_index, id: id, **options)
+      fetch_resource(:participation_participants_index, id: id, **options)
+    end
+
+    private
+
+    def fetch_resource(resource_key, **params)
+      if params.any?
+        Hal.instance.register.fetch(resource_key, **params)
+      else
+        Hal.instance.register.fetch(resource_key)
+      end
     end
   end
 end
