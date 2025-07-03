7ed1423f938209ae69a91a6451d9c132ffd4016d
Ronald Tse <ronald.tse@ribose.com>
2025-07-04 00:46:57 +0800

    fix: {predecessor,successor}-version values are SpecVersion not the Index

--- lib/w3c_api/models/spec_version_index.rb
+++ lib/w3c_api/models/spec_version_index.rb
@@ -5,9 +5,10 @@ require_relative 'spec_version'
 module W3cApi
   module Models
     class SpecVersionIndex < Lutaml::Hal::Page
+      hal_link :versions, key: 'versions', realize_class: 'SpecVersion', collection: true
       hal_link :spec_versions, key: 'version-history', realize_class: 'SpecVersion', collection: true
-      hal_link :predecessor_version, key: 'predecessor-version', realize_class: 'SpecVersionIndex', collection: true
-      hal_link :successor_version, key: 'successor-version', realize_class: 'SpecVersionIndex', collection: true
+      hal_link :predecessor_version, key: 'predecessor-version', realize_class: 'SpecVersion', collection: true
+      hal_link :successor_version, key: 'successor-version', realize_class: 'SpecVersion', collection: true
     end
   end
 end
